74b1330bc1816cc57cf31a39980fc81d3aa6916e
Felipe Escárate <felipeescaratefernandez@gmail.com>
2024-04-03 00:57:59 -0300

    wip: update env_requirements

--- prefab_generation/env_requirements.sh
+++ prefab_generation/env_requirements.sh
@@ -1,26 +1,14 @@
-# COLMAP installation
-sudo apt-get install \
-    git \
-    cmake \
-    ninja-build \
-    build-essential \
-    libboost-program-options-dev \
-    libboost-filesystem-dev \
-    libboost-graph-dev \
-    libboost-system-dev \
-    libeigen3-dev \
-    libflann-dev \
-    libfreeimage-dev \
-    libmetis-dev \
-    libgoogle-glog-dev \
-    libgtest-dev \
-    libsqlite3-dev \
-    libglew-dev \
-    qtbase5-dev \
-    libqt5opengl5-dev \
-    libcgal-dev \
-    libceres-dev
+# CUDA installation
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-11-8
 
+source ~/.bashrc
+# COLMAP installation
+sudo apt-get -y install git cmake ninja-build build-essential libboost-program-options-dev libboost-filesystem-dev libboost-graph-dev libboost-system-dev libeigen3-dev libflann-dev libfreeimage-dev libmetis-dev libgoogle-glog-dev libgtest-dev libsqlite3-dev libglew-dev qtbase5-dev libqt5opengl5-dev libcgal-dev libceres-dev
+
+source ~/.bashrc
 git clone https://github.com/colmap/colmap.git
 cd colmap
 mkdir build
@@ -29,15 +17,11 @@ cmake .. -GNinja
 ninja
 sudo ninja install
 
-# CUDA installation
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
-sudo dpkg -i cuda-keyring_1.1-1_all.deb
-sudo apt-get update
-sudo apt-get -y install cuda-toolkit-11-8
-
+source ~/.bashrc
 # Miniconda installation
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 
+source ~/.bashrc
 # For OpenCV
 sudo apt-get install ffmpeg libsm6 libxext6  -y
