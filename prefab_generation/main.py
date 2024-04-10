266a36c6e7c67d9f4155b8bb923575d65f9ba7ff
Felipe Escárate <felipeescaratefernandez@gmail.com>
2024-04-09 23:16:20 -0400

    feat: add endpoint for mesh generation

--- prefab_generation/main.py
+++ prefab_generation/main.py
@@ -1,29 +1,36 @@
-from media import video_to_photos, try_to_create_dir
-from gaussian_splatting import gSplatModel
+import os, time
+from fastapi import FastAPI, File, UploadFile, BackgroundTasks
+from media import video_to_images, try_to_create_dir
+from gaussian_splatting import generate_mesh
 from dotenv import load_dotenv
-import os
-
 load_dotenv()
+
 VIDEOS_PATH = os.environ['VIDEOS_PATH']
 COLMAPS_PATH = os.environ['COLMAPS_PATH']
-GAUSSIANS_PATH = os.environ['GAUSSIANS_PATH']
-MESHES_PATH = os.environ['MESHES_PATH']
+N_FRAMES = os.environ['N_FRAMES']
 
-g_splat = gSplatModel(GAUSSIANS_PATH, MESHES_PATH)
+app = FastAPI()
 
-def video_to_asset(index, n_frames = 15):
-    # Create name and folders
-    name = f"{index}_{n_frames}F"
+@app.post("/get_mesh_from_video/{index}/")
+async def get_mesh_from_video(index: int, background_tasks: BackgroundTasks, video: UploadFile = File(...)):
+    # Create name
+    name = f"{index}_{N_FRAMES}F"
+    extension = 'mp4' # The extension should be get from the database
+    print(f"Saving video {name}.{extension}")
+    video_path = os.path.join(VIDEOS_PATH, f"{name}.{extension}")
+    with open(video_path, "wb") as f:
+        f.write(video.file.read())
+    print("Creating directories...")
     colmap_path = os.path.join(COLMAPS_PATH, name)
     try_to_create_dir(colmap_path)
     images_path = os.path.join(colmap_path, "input")
     try_to_create_dir(images_path)
-    # Get images from video
-    video_to_photos(index, n_frames, VIDEOS_PATH, images_path)
-    # Generate OBJ using Gaussian Splatting and SuGaR
-    run_res = g_splat.run(name, colmap_path, remove_backgrounds=False)
-    return run_res
 
-if __name__ == "__main__":
-    video_to_asset(1, 15)
-    print("Done!")
+    try:
+        print("Extracting images from video...")
+        video_to_images(video_path, images_path)
+    except Exception as e:
+        return {"status": "error", "message": f"Error extracting images from video: {e}"}
+    # Generate OBJ using Gaussian Splatting and Gaustudio
+    background_tasks.add_task(generate_mesh, index, name, colmap_path)
+    return {"status": "success", "message": "Mesh generation started in background"}
--- prefab_generation/media.py
+++ prefab_generation/media.py
@@ -1,7 +1,6 @@
 import os
 import cv2
 from sys import argv
-from gaussian_splatting import gSplatModel
 
 def try_to_create_dir(path):
     try:
@@ -9,23 +8,11 @@ def try_to_create_dir(path):
     except FileExistsError:
         print(f"The directory {path} already exists")
 
-def video_to_photos_ffmpeg(input, output_path, qscale=1, qmin=1, fps=2):
-    # Creating the output directory
-    path = os.path.join(output_path, input.split(".")[0])
-    try_to_create_dir(path)
-    imgs_path = os.path.join(path, "input")
-    try_to_create_dir(imgs_path)
-    os.system(f"ffmpeg -i videos\{input} -qscale:v {qscale} -qmin {qmin} -vf fps={fps} {imgs_path}\%04d.jpg")
-    return path
-
-def video_to_photos(index: int, n_frames: int, video_path: str, images_path: str) -> None:
-    extension = 'mp4' # The extension should be get from the database
+def video_to_images(video_path: str, images_path: str) -> None:
     # Open the video
-    vidcap = cv2.VideoCapture(f"{video_path}\{index}.{extension}")
+    vidcap = cv2.VideoCapture(video_path)
     # Get total number of frames
     total_frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
-    # Calculate spacing between frames
-    frame_spacing = max(total_frames // n_frames, 1)
     
     # Iterate through the video frames
     count = 0
@@ -33,11 +20,9 @@ def video_to_photos(index: int, n_frames: int, video_path: str, images_path: str
     while count < total_frames and success:
         vidcap.set(cv2.CAP_PROP_POS_FRAMES, count) # Set frame position
         success, image = vidcap.read()
-        if count % frame_spacing == 0:
-            # Save the frame as an image file
-            frame_filename = os.path.join(images_path, f"frame_{count:04d}.jpg")
-            cv2.imwrite(frame_filename, image)
-        
-        count += frame_spacing
+        # Save the frame as an image file
+        frame_filename = os.path.join(images_path, f"frame_{count:04d}.jpg")
+        cv2.imwrite(frame_filename, image)
+        count += 1
 
     vidcap.release()
