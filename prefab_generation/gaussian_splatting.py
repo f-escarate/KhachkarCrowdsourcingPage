96cb441a676d2d6b53d57d0253cc29b54c161502
Felipe Escárate <felipeescaratefernandez@gmail.com>
2024-04-09 23:22:42 -0400

    change: use index to set the folder's name

--- prefab_generation/gaussian_splatting.py
+++ prefab_generation/gaussian_splatting.py
@@ -14,24 +14,24 @@ def remove_backgrounds(colmap_path):
     for photo in photos:
         remove_background(photo, colmap_path, cleaned_path)
 
-def generate_mesh(index: int, name: str, colmap_path: str, remove_backgrounds: bool = True):
+def generate_mesh(index: int, colmap_path: str, remove_backgrounds: bool = True):
     convert_res = os.system(f'python {GAUSSIAN_SPLATTING_PATH}/convert.py -s {colmap_path}')
     if convert_res != 0:
         print("COLMAP Error")
         return 1
     if remove_backgrounds:
         remove_backgrounds(colmap_path)
-    train_res = os.system(f'python {GAUSSIAN_SPLATTING_PATH}/train.py -s {colmap_path} --iterations 7000 --model_path {GAUSSIANS_PATH}/{name}')
+    train_res = os.system(f'python {GAUSSIAN_SPLATTING_PATH}/train.py -s {colmap_path} --iterations 7000 --model_path {GAUSSIANS_PATH}/{index}')
     if train_res != 0:
         print("Training Gaussians Error")
         return 2
     # Generate the mesh using gaustudio
-    gaustudio_res = os.system(f'gs-extract-mesh -m {GAUSSIANS_PATH}/{name} -o {MESHES_PATH}/{name}')
+    gaustudio_res = os.system(f'gs-extract-mesh -m {GAUSSIANS_PATH}/{index} -o {MESHES_PATH}/{index}')
     if gaustudio_res != 0:
         print("Gaustudio Error")
         return 3
     # Bind the texture using mvs-texturing
-    mvs_res = os.system(f'cd {MESHES_PATH}/{name} && texrecon ./images ./fused_mesh.ply ./textured_mesh --outlier_removal=gauss_clamping --data_term=area --no_intermediate_results')
+    mvs_res = os.system(f'cd {MESHES_PATH}/{index} && texrecon ./images ./fused_mesh.ply ./textured_mesh --outlier_removal=gauss_clamping --data_term=area --no_intermediate_results')
     if mvs_res != 0:
         print("MVS Texturing Error")
         return 4
--- prefab_generation/main.py
+++ prefab_generation/main.py
@@ -1,4 +1,4 @@
-import os, time
+import os
 from fastapi import FastAPI, File, UploadFile, BackgroundTasks
 from media import video_to_images, try_to_create_dir
 from gaussian_splatting import generate_mesh
@@ -7,21 +7,19 @@ load_dotenv()
 
 VIDEOS_PATH = os.environ['VIDEOS_PATH']
 COLMAPS_PATH = os.environ['COLMAPS_PATH']
-N_FRAMES = os.environ['N_FRAMES']
 
 app = FastAPI()
 
 @app.post("/get_mesh_from_video/{index}/")
 async def get_mesh_from_video(index: int, background_tasks: BackgroundTasks, video: UploadFile = File(...)):
     # Create name
-    name = f"{index}_{N_FRAMES}F"
-    extension = 'mp4' # The extension should be get from the database
-    print(f"Saving video {name}.{extension}")
-    video_path = os.path.join(VIDEOS_PATH, f"{name}.{extension}")
+    extension = 'mp4'
+    print(f"Saving video {index}.{extension}")
+    video_path = os.path.join(VIDEOS_PATH, f"{index}.{extension}")
     with open(video_path, "wb") as f:
         f.write(video.file.read())
     print("Creating directories...")
-    colmap_path = os.path.join(COLMAPS_PATH, name)
+    colmap_path = os.path.join(COLMAPS_PATH, str(index))
     try_to_create_dir(colmap_path)
     images_path = os.path.join(colmap_path, "input")
     try_to_create_dir(images_path)
@@ -32,5 +30,5 @@ async def get_mesh_from_video(index: int, background_tasks: BackgroundTasks, vid
     except Exception as e:
         return {"status": "error", "message": f"Error extracting images from video: {e}"}
     # Generate OBJ using Gaussian Splatting and Gaustudio
-    background_tasks.add_task(generate_mesh, index, name, colmap_path)
+    background_tasks.add_task(generate_mesh, index, colmap_path)
     return {"status": "success", "message": "Mesh generation started in background"}
