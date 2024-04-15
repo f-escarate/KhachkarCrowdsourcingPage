af1f7d10ac076499e2e80ca374e808e783386b98
Felipe Escárate <felipeescaratefernandez@gmail.com>
2024-04-14 21:20:36 -0400

    chore: set host URL depending on 'dev' or 'build'

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,4 +1,5 @@
-export const HOST = 'http://localhost:8000';
+const { MODE } = import.meta.env;
+export const HOST = MODE === 'development' ? 'http://localhost:8000' : 'https://saduewa.dcc.uchile.cl/crowdsourcing_backend';
 export const MUSEUM_URL = 'https://saduewa.dcc.uchile.cl/museum/';
 export const ONLINE_MUSEUM_URL = 'https://saduewa.dcc.uchile.cl/online-museum/';
 export const TEXT_FIELDS_WO_DATE = {
