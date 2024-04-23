7aae312181b5d7f774d334e9726913092fc1f32c
Felipe Escárate <felipeescaratefernandez@gmail.com>
2024-04-23 19:09:00 -0400

    feat: add slot to put buttons

--- frontend/src/components/Entry.svelte
+++ frontend/src/components/Entry.svelte
@@ -47,57 +47,62 @@
     }
 </script>
 
-<div class='m-2 p-4 border-b-2 border-amber-500 md:flex md:justify-between md:max-h-[300px] md:w-[85%]'>
-    <div class='m-2 flex flex-col justify-between items-center md:items-start'>
-        <h1 class='text-4xl font-semibold'>{entry_data.location} {entry_data.id}</h1>
-        <img class='max-h-[50vh] md:max-h-full rounded-lg md:rounded-none md:hidden w-full m-4 object-contain' src={image} alt={entry_data.id} />
-        <p class='m-2'>{entry_data.inscription}</p>
-        <p class="m-2 text-xs font-bold">Upload date {entry_data.date}</p>
-        <div id='buttons_container' class='flex self-center'>
-            <Button on:click={previewData} size="xs" class='m-2 bg-amber-500'>
-                <ListIcon sx='m-0 mr-1 text-white'/>
-                Preview Data
-            </Button>
-            {#if entry_data.state === 'processing_video'}
-                <h3>Khachkar video is being processed</h3>
-            {:else if entry_data.state === 'meshed'}
-                <Button on:click={window.open(`${base}/viewMesh/${entry_data.id}`,'_blank','noopener')} size="xs" class='m-2 bg-amber-500'>
-                    <SquareIcon sx='m-0 mr-1 text-white'/>
-                    Preview Mesh
+<div class='m-2 md:w-[85%] flex flex-col border-b-2 border-amber-500 items-center w-auto'>
+    <div class='p-4 md:flex md:justify-between md:max-h-[300px]'>
+        <div class='m-2 flex flex-col justify-between items-center md:items-start'>
+            <h1 class='text-4xl font-semibold'>{entry_data.location} {entry_data.id}</h1>
+            <img class='max-h-[50vh] md:max-h-full rounded-lg md:rounded-none md:hidden w-full m-4 object-contain' src={image} alt={entry_data.id} />
+            <p class='m-2'>{entry_data.inscription}</p>
+            <p class="m-2 text-xs font-bold">Upload date {entry_data.date}</p>
+            <div id='buttons_container' class='flex self-center'>
+                <Button on:click={previewData} size="xs" class='m-2 bg-amber-500'>
+                    <ListIcon sx='m-0 mr-1 text-white'/>
+                    Preview Data
                 </Button>
-            {:else}
-                <Button on:click={previewVideo} size="xs" class='m-2 bg-amber-500'>
-                    <VideoIcon sx='m-0 mr-1 text-white'/>
-                    Preview Video
-                </Button>
-                <Modal title="video" bind:open={clickOutsideVideoModal} autoclose outsideclose>
-                    <video id="videoElement" controls class="w-full max-h-[100%]">
-                        <source type="video/mp4" id='videoDiv'>
-                        <track kind="captions">
-                        Your browser does not support the video tag.
-                    </video>
-                </Modal>
-            {/if}
+                {#if entry_data.state === 'processing_video'}
+                    <h3>Khachkar video is being processed</h3>
+                {:else if entry_data.state === 'meshed'}
+                    <Button on:click={window.open(`${base}/viewMesh/${entry_data.id}`,'_blank','noopener')} size="xs" class='m-2 bg-amber-500'>
+                        <SquareIcon sx='m-0 mr-1 text-white'/>
+                        Preview Mesh
+                    </Button>
+                {:else}
+                    <Button on:click={previewVideo} size="xs" class='m-2 bg-amber-500'>
+                        <VideoIcon sx='m-0 mr-1 text-white'/>
+                        Preview Video
+                    </Button>
+                {/if}
+            </div>
         </div>
+        <img class='hidden md:block w-1/2 object-contain' src={image} alt={entry_data.id} />
     </div>
-    <img class='hidden md:block w-1/2 object-contain' src={image} alt={entry_data.id} />
-    <Modal title="Khachkar information" bind:open={clickOutsideModal} autoclose outsideclose>
-        <Table striped={true}>
-            <TableHead>
-                <TableHeadCell>Field</TableHeadCell>
-                <TableHeadCell>Value</TableHeadCell>
-            </TableHead>
-            <TableBody tableBodyClass="divide-y">
-                {#each FIELDS as key}
-                    <TableBodyRow>
-                        <TableBodyCell>{TEXT_FIELDS[key]}</TableBodyCell>
-                        <TableBodyCell>{entry_data[key]}</TableBodyCell>
-                    </TableBodyRow>
-                {/each}
-            </TableBody>
-        </Table>
-        <svelte:fragment slot="footer">
-            <Button color="alternative">Back</Button>
-        </svelte:fragment>
-    </Modal>
+    <slot class='mx-auto'></slot>
 </div>
+
+<Modal title="Khachkar information" bind:open={clickOutsideModal} autoclose outsideclose>
+    <Table striped={true}>
+        <TableHead>
+            <TableHeadCell>Field</TableHeadCell>
+            <TableHeadCell>Value</TableHeadCell>
+        </TableHead>
+        <TableBody tableBodyClass="divide-y">
+            {#each FIELDS as key}
+                <TableBodyRow>
+                    <TableBodyCell>{TEXT_FIELDS[key]}</TableBodyCell>
+                    <TableBodyCell>{entry_data[key]}</TableBodyCell>
+                </TableBodyRow>
+            {/each}
+        </TableBody>
+    </Table>
+    <svelte:fragment slot="footer">
+        <Button color="alternative">Back</Button>
+    </svelte:fragment>
+</Modal>
+
+<Modal title="video" bind:open={clickOutsideVideoModal} autoclose outsideclose>
+    <video id="videoElement" controls class="w-full max-h-[100%]">
+        <source type="video/mp4" id='videoDiv'>
+        <track kind="captions">
+        Your browser does not support the video tag.
+    </video>
+</Modal>
