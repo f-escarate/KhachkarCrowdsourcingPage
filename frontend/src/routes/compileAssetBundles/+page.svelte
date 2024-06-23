5c625d0703f00db7a64c824e60fc8903c4dd9e49
Felipe Escárate <felipeescaratefernandez@gmail.com>
2024-06-22 20:16:53 -0400

    feat: add display info and display mesh buttons to compile Asset Bundles section

--- frontend/src/routes/compileAssetBundles/+page.svelte
+++ frontend/src/routes/compileAssetBundles/+page.svelte
@@ -1,6 +1,10 @@
 <script>
     import { onMount } from 'svelte';
     import { Table, TableBody, TableHead, TableBodyCell, TableHeadCell, TableBodyRow, Checkbox, Button, Spinner } from 'flowbite-svelte';
+    import KhachkarInfoModal from '../../components/KhachkarInfoModal.svelte'
+    import EyeIcon from '../../components/icons/EyeIcon.svelte';
+    import ListIcon from '../../components/icons/ListIcon.svelte';
+    import SquareIcon from '../../components/icons/SquareIcon.svelte';
     import { HOST } from '$lib/constants';
     import { base } from '$app/paths';
     import { auth_get_json, auth_post_request } from '$lib/utils';
@@ -54,39 +58,62 @@
             checkboxes[i].checked = e.target.checked;
         }
     }
-
-
+    let clickOutsideModal = false;
+    let entry_data = {};
 </script>
 
-<h1 class='m-4 text-4xl font-bold'>Compile Asset Bundles</h1>
+<KhachkarInfoModal entry_data={entry_data} bind:clickOutsideModal={clickOutsideModal} />
+<h1 class='text-4xl font-bold'>Manage khachkars in the museum</h1>
 <div class='flex flex-col'>
     {#if entries.length > 0}
-        <Table striped={true}>
-            <TableHead>
-                <TableHeadCell>Khachkar</TableHeadCell> 
-                <TableBodyCell><Checkbox on:click={handleSelectAll} /></TableBodyCell>
+        <h2 class='my-2 text-xl font-semibold text-amber-600'>Khachkars to add </h2>
+        <Table striped={true} shadow={true} class='border-4 border-amber-400 rounded-lg'>
+            <TableHead theadClass='text-xs uppercase align-text-top'>
+                <TableHeadCell padding='px-3 py-3'>Khachkar</TableHeadCell> 
+                <TableHeadCell padding='px-1 py-3'><EyeIcon sx="inline mr-1"/>View info</TableHeadCell>
+                <TableHeadCell padding='px-1 py-3'><EyeIcon sx="inline mr-1"/>View mesh</TableHeadCell>
+                <TableHeadCell padding='px-3 py-3' class='h-full align-middle'>
+                    <Checkbox inline on:click={handleSelectAll}>
+                        <p class='text-xs uppercase align-text-top font-bold'>Select all</p>
+                    </Checkbox>
+                </TableHeadCell>
             </TableHead>
             <TableBody tableBodyClass="divide-y">
                 {#each entries as entry}
                     <TableBodyRow>
-                        <TableBodyCell>{entry['location']} {entry['id']}</TableBodyCell>
-                        <TableBodyCell><Checkbox name='checkbox' /></TableBodyCell>
+                        <TableBodyCell tdClass='px-3 py-4 font-medium'>
+                            {`${entry['id']}) ${entry['location']}`}
+                        </TableBodyCell>
+                        <TableBodyCell tdClass='px-1 py-4'>
+                            <Button on:click={async () => {clickOutsideModal = true; entry_data=entry;}} size="xs" class='w-full bg-amber-500'>
+                                <ListIcon sx='text-white'/>
+                            </Button>
+                        </TableBodyCell>
+                        <TableBodyCell tdClass='px-1 py-4'>
+                            <Button on:click={window.open(`${base}/viewMesh/${entry.id}`,'_blank','noopener')} size="xs" class='w-full bg-orange-500'>
+                                <SquareIcon sx='text-white'/>
+                            </Button>
+                        </TableBodyCell>
+                        <TableBodyCell tdClass='px-3 py-4'>
+                            <Checkbox name='checkbox'/>
+                        </TableBodyCell>
                     </TableBodyRow>
                 {/each}
             </TableBody>
         </Table>
         {#if compiling_asset_bundles}
             <p class='m-2 font-bold text-orange-700'>This process could take a few minutes</p>
-            <Button disabled size="xs" class='m-2 bg-amber-500'>
+            <Button disabled class='self-end max-w-[400px] my-4 bg-amber-500'>
                 <Spinner class="mr-2" size="4"/>
                 Compiling asset bundles...
             </Button>
-            
         {:else}
-            <Button on:click={handleCompilation} size="xs" class='m-2 bg-amber-500'>Compile Asset Bundles</Button>
+            <Button on:click={handleCompilation} class='self-end max-w-[400px] my-4 bg-amber-500'>
+                Add selected khachkars to the museum
+            </Button>
         {/if}
         
     {:else}
-        <p class='m-4'>No entries found</p>
+        <h2 class='m-4'>No entries found</h2>
     {/if}
 </div>
