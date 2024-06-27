7f746bc8db575a28f20ff69d45e9f93380168ccd
Felipe Escárate <felipeescaratefernandez@gmail.com>
2024-06-27 00:58:28 -0400

    feat: add a 'How to upload a mesh' tutorial, and change styles

--- frontend/src/components/FormComponents/VideoOrMesh.svelte
+++ frontend/src/components/FormComponents/VideoOrMesh.svelte
@@ -1,14 +1,19 @@
 <script>
-    import { Label, Button, Radio, Dropdown, Popover } from 'flowbite-svelte';
+    import { Label, Button, Radio, Dropdown, Popover, Modal } from 'flowbite-svelte';
     import { BASE_MESH_DATA } from '$lib/constants';
+    import { base } from '$app/paths';
     import DownIcon from '../icons/DownIcon.svelte';
     import VideoIcon from '../icons/VideoIcon.svelte';
     import SquareIcon from '../icons/SquareIcon.svelte';
     import QuestionIcon from '../icons/QuestionIcon.svelte';
+    import ClipIcon from '../icons/ClipIcon.svelte';
+    import VideoFileIcon from '../icons/VideoFileIcon.svelte';
     export let entry = {
         video: null
     };
     let videoVisibility = 0;
+    let dropdownOpen = false;
+    let modal = false;
     let meshData = {... BASE_MESH_DATA};
     const previewFile = (elementID, file) => {
         let preview = document.getElementById(elementID);
@@ -73,34 +78,31 @@
         <div class='flex gap-1 items-center font-semibold h-full'>
             Select between Video or Mesh
             <div id='whatIsAMesh'>
-                <QuestionIcon  sx='w-6 h-6 text-cyan-500'/>
+                <QuestionIcon  sx='w-6 h-6 text-amber-500'/>
                 <Popover class="w-64 text-sm " title="What is a mesh?" triggeredBy='#whatIsAMesh'>
                     A mesh is a representation of a 3D object. In this case is a 3D model of the khachkar.
                 </Popover>
             </div>
         </div>     
         <Button class='bg-amber-500 hover:bg-amber-700 mb-4'>
-            Selected: <p class='font-bold'>{meshData.withMesh? 'Mesh': 'Video'}</p> <DownIcon sx='m-0 text-white'/>
+            Selected: <p class='ml-1 font-bold'>{meshData.withMesh? 'Mesh': 'Video'}</p> <DownIcon sx='m-0 text-white'/>
         </Button>
-        <Dropdown class="w-44 p-3 space-y-3 text-sm">
-            <Radio class='p-1 rounded hover:bg-amber-200' name="wMesh" bind:group={meshData.withMesh} on:change={e => handleToggle()} value={false}>
+        <Dropdown class="w-44 p-3 space-y-3 text-sm" bind:open={dropdownOpen}>
+            <Radio class='p-1 rounded hover:bg-amber-200' name="wMesh" bind:group={meshData.withMesh} on:click={() => (dropdownOpen = false)} on:change={e => handleToggle()} value={false}>
                 <VideoIcon sx='mx-1 text-black'/> Video
             </Radio>
-            <Radio class='p-1 rounded hover:bg-amber-200' name="wMesh" bind:group={meshData.withMesh} on:change={e => handleToggle()} value={true}>
+            <Radio class='p-1 rounded hover:bg-amber-200' name="wMesh" bind:group={meshData.withMesh} on:click={() => (dropdownOpen = false)} on:change={e => handleToggle()} value={true}>
                 <SquareIcon sx='mx-1 text-black'/> Mesh
             </Radio>
         </Dropdown>
     </div>
     {#if meshData.withMesh}
-        <div class='flex mb-2'>Upload mesh <SquareIcon sx='mx-1 text-black'/>
-            <div id='howToUploadAMesh'>
-                <QuestionIcon  sx='w-6 h-6 text-red-800'/>
-                <Popover class="w-64 text-sm " title="What is a mesh?" triggeredBy='#howToUploadAMesh'>
-                    It requires a .obj file, a .mtl file and one or more texture images. If there are multiple images, they must be uploaded all at once.
-                </Popover>
-            </div>
-        
-        
+        <div class='inline-block mb-2 align-middle'>
+            <SquareIcon sx='text-black inline'/>
+            Upload a mesh
+            <Button size="xs" class='bg-cyan-400 hover:bg-cyan-600 ml-2 my-auto' on:click={e => modal=true}>
+                <p class='font-bold'>How to upload a mesh?</p>
+            </Button>
         </div>
         <div class="mb-2 w-full flex flex-col gap-2">
             <label for="mesh" class='font-bold text-sm'>Mesh file (.obj)</label>
@@ -110,21 +112,32 @@
             <label for="mesh_images" class='font-bold text-sm'>Texture files (multiple images)</label>
             <input type="file" class="mx-4 w-full" on:change={e => meshData.images=e.target.files} id="mesh_images" name="mesh_images" accept="image/*" multiple>
         </div>
+        <Modal bind:open={modal} title='How to upload a mesh?' width='w-1/2' autoclose outsideclose>
+            <p class='text'>To upload a mesh you need to have the following files:</p>
+            <ul class='list-disc list-inside'>
+                <li class='text-sm'>A .obj file that contains the 3D model.</li>
+                <li class='text-sm'>A .mtl file that contains the material properties.</li>
+                <li class='text-sm'>One or more image files that contain the textures.</li>
+            </ul>
+            <b class='text-amber-500'>Important: The images must be added all at once.</b>
+            <img src={`${base}/gifs/gifUploadMesh.gif`} alt='Upload mesh example' class='w-full'/>
+        </Modal>
     {:else}
-        <p class='flex'>Upload video <VideoIcon sx='mx-1 text-black'/></p>
+        <p class='flex'><VideoFileIcon sx='w-6 h-6 text-black'/> Upload a video</p>
         <Label for="video" class="mb-2 w-full">
-            <div class="my-4 p-1 flex border-4 border-amber-300 hover:bg-amber-500 text-center hover:text-white transition-colors duration-400 ease-in-out">
+            <div class="rounded-xl my-4 p-1 flex border-4 border-amber-300 hover:bg-amber-500 hover:cursor-pointer text-center hover:text-white transition-colors duration-400 ease-in-out">
                 <video class={(videoVisibility? 'visible': 'hidden') + ' md:w-1/2 m-auto p-2'} id='videoElement' controls>
                 <source id="previewVideo">
                     <track kind="captions"/>
                     Your browser does not support HTML5 video.
                 </video>
-                <p class={(videoVisibility? 'hidden': 'visible') + ' self-center mx-auto'}>Upload Video</p>
+                <p class={(videoVisibility? 'hidden': 'visible') + ' self-center mx-auto inline-flex'}>
+                    <ClipIcon sx='w-6 h-6'/>
+                    Click to select a video...
+                </p>
                 <input type="file" id="video" name="video" class="w-0 invisible" on:change={loadVideo} accept="video/*">
             </div>
         </Label>
-    {/if}   
-
-
+    {/if}
 </div>
 
